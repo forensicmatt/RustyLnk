d7f7ee09d785c67327a6659aa1f53c7e8c10b6f3
matthew seyer <matthew.seyer@gmail.com>
2017-07-02 15:20:14 -0500

    Added and renamed string reading help methods

--- src/lnkpkg/utils.rs
+++ src/lnkpkg/utils.rs
@@ -6,7 +6,39 @@ use std::io::Read;
 use std::io::Error;
 use std::slice;
 
-pub fn read_string_utf8<R: Read>(mut reader: R) -> Result<String,LnkError> {
+pub fn read_string_u8_w_size<R: Read>(mut reader: R) -> Result<String,LnkError> {
+    // Reads into a string with the size preceeding the string
+    let str_size = reader.read_u8()?;
+
+    let mut utf8_buffer = vec![0;str_size as usize];
+    reader.read_exact(utf8_buffer.as_mut_slice())?;
+
+    Ok(
+        String::from_utf8(utf8_buffer)?
+    )
+}
+
+pub fn read_string_u16_w_size<R: Read>(mut reader: R) -> Result<String,LnkError> {
+    // Reads into a string with the size preceeding the string
+    // multiply size by two (wchar = 2 bytes)
+    let str_size = reader.read_u16::<LittleEndian>()? * 2;
+
+    let mut utf16_buffer = vec![0;str_size as usize];
+    reader.read_exact(utf16_buffer.as_mut_slice())?;
+
+    let utf16_string = match UTF_16LE.decode(&utf16_buffer.as_slice(),DecoderTrap::Ignore) {
+        Ok(utf16) => utf16,
+        Err(error) => return Err(
+            LnkError::utf16_decode_error(
+                format!("Error decoding name in volume_label_unicode field. [{}]",error)
+            )
+        )
+    };
+
+    Ok(utf16_string)
+}
+
+pub fn read_string_u8_till_null<R: Read>(mut reader: R) -> Result<String,LnkError> {
     // Reads into a string till a null char is reached
     let utf8_buffer = get_u8_vec(&mut reader)?;
     Ok(
@@ -14,7 +46,7 @@ pub fn read_string_utf8<R: Read>(mut reader: R) -> Result<String,LnkError> {
     )
 }
 
-pub fn read_string_utf16<R: Read>(mut reader: R) -> Result<String,LnkError> {
+pub fn read_string_u16_till_null<R: Read>(mut reader: R) -> Result<String,LnkError> {
     // Reads into a string till a null char is reached
     let utf16_buffer = get_u8_vec_utf16(&mut reader)?;
     let utf16_string = match UTF_16LE.decode(&utf16_buffer.as_slice(),DecoderTrap::Ignore) {
