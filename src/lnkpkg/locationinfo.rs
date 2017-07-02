5b3eb81261d7b8c9dc200b04c0775c9baa164200
matthew seyer <matthew.seyer@gmail.com>
2017-07-02 15:23:30 -0500

    Added local path and common path strings to LocationInfo struct

--- src/lnkpkg/locationinfo.rs
+++ src/lnkpkg/locationinfo.rs
@@ -11,15 +11,17 @@ use std::io::Seek;
 
 #[derive(Debug)]
 pub struct LocationInfo {
-    pub info_size: u32,
-    pub header_size: u32,
-    pub flags: u32,
-    pub offset_vol_info: u32,
-    pub offset_loc_path: u32,
-    pub offset_net_share: u32,
-    pub offset_common_path: u32,
-    pub volume_info: VolumeInfo,
-    pub netshare_info: NetworkShareInfo
+    info_size: u32,
+    header_size: u32,
+    flags: u32,
+    offset_vol_info: u32,
+    offset_loc_path: u32,
+    offset_net_share: u32,
+    offset_common_path: u32,
+    volume_info: VolumeInfo,
+    local_path: String,
+    netshare_info: NetworkShareInfo,
+    common_path: String
 }
 impl LocationInfo {
     pub fn new<Rs: Read + Seek>(mut reader: Rs) -> Result<LocationInfo,LnkError> {
@@ -41,11 +43,23 @@ impl LocationInfo {
         )?;
         let volume_info = VolumeInfo::new(&mut reader)?;
 
+        // read local path
+        reader.seek(
+            SeekFrom::Start(_offset + offset_loc_path as u64)
+        )?;
+        let local_path = utils::read_string_u8_till_null(&mut reader)?;
+
         // read netshare info
         reader.seek(
             SeekFrom::Start(_offset + offset_net_share as u64)
         )?;
         let netshare_info = NetworkShareInfo::new(&mut reader)?;
+
+        // read common path
+        reader.seek(
+            SeekFrom::Start(_offset + offset_common_path as u64)
+        )?;
+        let common_path = utils::read_string_u8_till_null(&mut reader)?;
 
         // seek to end of location info
         reader.seek(
@@ -62,7 +76,9 @@ impl LocationInfo {
                 offset_net_share: offset_net_share,
                 offset_common_path: offset_common_path,
                 volume_info: volume_info,
-                netshare_info: netshare_info
+                local_path: local_path,
+                netshare_info: netshare_info,
+                common_path: common_path
             }
         )
     }
