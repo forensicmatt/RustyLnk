f24830d1199306aec0e1b992ac3820d34ac5c9cb
matthew seyer <matthew.seyer@gmail.com>
2017-07-01 20:06:02 -0500

    Added help method to get u8 vector of utf16 byte string with null end

--- src/lnkpkg/utils.rs
+++ src/lnkpkg/utils.rs
@@ -1,6 +1,7 @@
 use byteorder::{ReadBytesExt, LittleEndian};
 use std::io::Read;
 use std::io::Error;
+use std::slice;
 
 pub fn get_u8_vec<R: Read>(mut reader: R) -> Result<Vec<u8>,Error> {
     let mut string_vec: Vec<u8> = Vec::new();
@@ -15,6 +16,25 @@ pub fn get_u8_vec<R: Read>(mut reader: R) -> Result<Vec<u8>,Error> {
     }
 
     Ok(string_vec)
+}
+
+pub fn get_u8_vec_utf16<R: Read>(mut reader: R) -> Result<Vec<u8>,Error>{
+    let mut u16_vec: Vec<u16> = Vec::new();
+
+    loop {
+        let u16_char = reader.read_u16::<LittleEndian>()?;
+        if u16_char == 0x0000 {
+            break
+        } else {
+            u16_vec.push(u16_char);
+        }
+    }
+
+    let u8_slice: &[u8] = unsafe {
+        slice::from_raw_parts(u16_vec.as_ptr() as *const u8, u16_vec.len() * 2)
+    };
+
+    Ok(u8_slice.to_vec())
 }
 
 pub fn get_u16_vec<R: Read>(mut reader: R) -> Result<Vec<u16>,Error> {
