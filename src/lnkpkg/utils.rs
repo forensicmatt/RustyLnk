7efdc3845d5b1751fda73b235d5e58b3885b132a
matthew seyer <matthew.seyer@gmail.com>
2017-07-02 14:21:04 -0500

    Added string read helper methods

--- src/lnkpkg/utils.rs
+++ src/lnkpkg/utils.rs
@@ -1,7 +1,32 @@
 use byteorder::{ReadBytesExt, LittleEndian};
+use encoding::all::UTF_16LE;
+use encoding::{Encoding, DecoderTrap};
+use lnkpkg::errors::{LnkError};
 use std::io::Read;
 use std::io::Error;
 use std::slice;
+
+pub fn read_string_utf8<R: Read>(mut reader: R) -> Result<String,LnkError> {
+    // Reads into a string till a null char is reached
+    let utf8_buffer = get_u8_vec(&mut reader)?;
+    Ok(
+        String::from_utf8(utf8_buffer)?
+    )
+}
+
+pub fn read_string_utf16<R: Read>(mut reader: R) -> Result<String,LnkError> {
+    // Reads into a string till a null char is reached
+    let utf16_buffer = get_u8_vec_utf16(&mut reader)?;
+    let utf16_string = match UTF_16LE.decode(&utf16_buffer.as_slice(),DecoderTrap::Ignore) {
+        Ok(utf16) => utf16,
+        Err(error) => return Err(
+            LnkError::utf16_decode_error(
+                format!("Error decoding name in volume_label_unicode field. [{}]",error)
+            )
+        )
+    };
+    Ok(utf16_string)
+}
 
 pub fn get_u8_vec<R: Read>(mut reader: R) -> Result<Vec<u8>,Error> {
     let mut string_vec: Vec<u8> = Vec::new();
