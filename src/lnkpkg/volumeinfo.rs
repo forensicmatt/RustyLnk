f59a7e01d1181072b9656faedcc93c900115d70b
forensicmatt <matthew.seyer@gmail.com>
2017-07-19 00:26:20 -0500

    Added DriveType and VolumeSerialNumber structs and serialization formating for VolumeInfo struct

--- src/lnkpkg/volumeinfo.rs
+++ src/lnkpkg/volumeinfo.rs
@@ -1,18 +1,95 @@
-use byteorder::{ReadBytesExt, LittleEndian};
+use byteorder::{ReadBytesExt, LittleEndian, ByteOrder};
+use serde::{ser};
 use lnkpkg::errors::{LnkError};
 use lnkpkg::utils;
+use std::mem::transmute;
+use std::fmt;
+use std::fmt::{Display,Debug};
 use std::io::SeekFrom;
 use std::io::Read;
 use std::io::Seek;
 
+pub struct VolumeSerialNumber(pub u32);
+impl VolumeSerialNumber{
+    pub fn to_string(&self)->String{
+        let buffer: [u8; 4] = unsafe {
+            transmute(self.0.to_le())
+        };
+
+        format!(
+            "{:04X}-{:04X}",
+            LittleEndian::read_u16(&buffer[2..4]),
+            LittleEndian::read_u16(&buffer[0..2])
+        )
+    }
+}
+impl Display for VolumeSerialNumber {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.to_string())
+    }
+}
+impl Debug for VolumeSerialNumber {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.to_string())
+    }
+}
+impl ser::Serialize for VolumeSerialNumber {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: ser::Serializer
+    {
+        serializer.serialize_str(
+            &self.to_string()
+        )
+    }
+}
+
+pub struct DriveType(pub u32);
+impl DriveType{
+    pub fn to_string(&self)->String{
+        match self.0 {
+            0 => "DRIVE_UNKNOWN".to_string(),
+            1 => "DRIVE_NO_ROOT_DIR".to_string(),
+            2 => "DRIVE_REMOVABLE".to_string(),
+            3 => "DRIVE_FIXED".to_string(),
+            4 => "DRIVE_REMOTE".to_string(),
+            5 => "DRIVE_CDROM".to_string(),
+            6 => "DRIVE_RAMDISK".to_string(),
+            _ => format!("UNKOWN_VALUE_0x{:08X}",self.0)
+        }
+    }
+}
+impl Display for DriveType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.to_string())
+    }
+}
+impl Debug for DriveType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.to_string())
+    }
+}
+impl ser::Serialize for DriveType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: ser::Serializer
+    {
+        serializer.serialize_str(
+            &self.to_string()
+        )
+    }
+}
+
 #[derive(Serialize,Debug)]
 pub struct VolumeInfo {
+    #[serde(skip_serializing)]
     pub vi_size: u32,
-    pub drive_type: u32,
-    pub serial_number: u32,
+    pub drive_type: DriveType,
+    pub serial_number: VolumeSerialNumber,
+    #[serde(skip_serializing)]
     pub offset_vol_label: u32,
+    #[serde(skip_serializing)]
     pub offset_vol_label_unicode: Option<u32>,
     pub volume_label: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub volume_label_unicode: Option<String>
 }
 
@@ -24,8 +101,8 @@ impl VolumeInfo {
         )?;
 
         let vi_size = reader.read_u32::<LittleEndian>()?;
-        let drive_type = reader.read_u32::<LittleEndian>()?;
-        let serial_number = reader.read_u32::<LittleEndian>()?;
+        let drive_type = DriveType(reader.read_u32::<LittleEndian>()?);
+        let serial_number = VolumeSerialNumber(reader.read_u32::<LittleEndian>()?);
         let offset_vol_label = reader.read_u32::<LittleEndian>()?;
 
         let mut offset_vol_label_unicode = None;
