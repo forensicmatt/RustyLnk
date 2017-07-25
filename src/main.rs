370dd7fa379aaeeb7142e952edee74108fa27f36
forensicmatt <matthew.seyer@gmail.com>
2017-07-24 22:39:02 -0500

    upped version

--- src/main.rs
+++ src/main.rs
@@ -130,7 +130,7 @@ fn main() {
         .help("JMES Query as bool only. (Prints whole record if true.)");
 
     let options = App::new("RusyLnk")
-        .version("0.0.0")
+        .version("0.1.0")
         .author("Matthew Seyer <https://github.com/forensicmatt/RustyUsn>")
         .about("LNK Parser written in Rust.")
         .arg(source_arg)
